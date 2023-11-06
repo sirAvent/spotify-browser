2d2bd1e77305244d38eefb65eb785d5c92dcee93
Avent Chiu <chiu7a@gmail.com>
2023-11-06 15:57:48 -0800

    Part 5

--- client/src/app/components/thermometer/thermometer.component.ts
+++ client/src/app/components/thermometer/thermometer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TrackFeature } from 'src/app/data/track-feature';
 
 @Component({
   selector: 'app-thermometer',
@@ -6,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thermometer.component.css']
 })
 export class ThermometerComponent implements OnInit {
-  //TODO: define Input fields and bind them to the template.
-
+  //DONE: define Input fields and bind them to the template.
+  @Input() trackFeature: TrackFeature;
   constructor() { }
 
   ngOnInit() {
--- client/src/app/pages/track-page/track-page.component.ts
+++ client/src/app/pages/track-page/track-page.component.ts
@@ -4,6 +4,7 @@ import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
 import { TrackFeature } from '../../data/track-feature';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-track-page',
@@ -15,11 +16,24 @@ export class TrackPageComponent implements OnInit {
 	track:TrackData;
   audioFeatures:TrackFeature[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
 
   ngOnInit() {
   	this.trackId = this.route.snapshot.paramMap.get('id');
-  	//TODO: Inject the spotifyService and use it to get the track data and it's audio features
-  }
+  	//DONE: Inject the spotifyService and use it to get the track data and it's audio features
+    this.trackId = this.route.snapshot.paramMap.get("id");
 
+    this.spotifyService.getTrack(this.trackId)
+    .then((track: TrackData) => {
+      this.track = track;
+    });
+
+    this.spotifyService.getAudioFeaturesForTrack(this.trackId)
+    .then((audioFeatures: TrackFeature[]) => {
+      this.audioFeatures = audioFeatures.filter(feature => TrackFeature.FeatureTypes.includes(feature.name));
+      for (let feature of this.audioFeatures) {
+        feature.id = this.trackId;
+      }
+    });
+  }
 }
